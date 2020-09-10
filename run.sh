b8f9d9c9d2fcad87c7a5a6c08e896a7b05abdc0c
Mohammad <mohammad4cj@gmail.com>
2020-09-10 18:58:57 +0100

    Clean up

--- run.sh
+++ run.sh
@@ -10,8 +10,18 @@ gcloud secrets versions access latest --secret=$name_secret --format='get(payloa
 gcloud auth activate-service-account --key-file ./creds/serviceaccount.json
 
 #https://cloud.google.com/sdk/gcloud/reference/run/deploy#--[no-]allow-unauthenticated
-gcloud run deploy $SERVICE_NAME --image gcr.io/$PROJECTid/$SERVICE_NAME:$COMMITsha --region $region --platform managed --allow-unauthenticated --port 80
+gcloud run deploy $SERVICE_NAME --image $img --region $region \
+  --platform managed \
+  --allow-unauthenticated \
+  --port 80
 
+# Clean up
+gcloud container images delete $img
+
+## To delete the Cloud Run service, use this command:
+# gcloud run services delete $SERVICE_NAME \
+#   --platform managed \
+#   --region $region
 
 ls
 
