5c88d5464f1f38f703dd99253caaf1e7bdf8ae91
Mohammad <mohammad4cj@gmail.com>
2020-09-10 21:07:16 +0100

    try faild error

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 
 echo " -------- Hello ----------"
 
-mkdir -p ./creds || exit 1
+mkdir -p ./creds
 
 #  Add secret "SERVICEACCOUNT_bigquery" to /security/secret-manager
 #  Enable "Secret Manager" in cloud-build/settings
-gcloud secrets versions access latest --secret=$name_secret --format='get(payload.data)' | tr '_-' '/+' | base64 -d > ./creds/serviceaccount.json || exit 0
+gcloud secrets versions access latest --secret=$name_secret --format='get(payload.data)' | tr '_-' '/+' | base64 -d > ./creds/serviceaccount.json
 
-gcloud auth activate-service-account --key-file ./creds/serviceaccount.json || exit 1
+gcloud auth activate-service-account --key-file ./creds/serviceaccount.json
 
 echo " -------- Cloud run ----------"
 #https://cloud.google.com/sdk/gcloud/reference/run/deploy#--[no-]allow-unauthenticated
